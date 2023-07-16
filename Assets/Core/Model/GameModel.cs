10175efe5989ab81650273f4d51fd5fe340a071d
Lev Zhybulia <l.zhybulia@kefirgames.com>
2023-07-16 22:29:49 +0300

    chg: move save controller to its own file

--- Assets/Core/Model/GameModel.cs
+++ Assets/Core/Model/GameModel.cs
@@ -1,25 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using Core.Model.Components;
 using Core.Model.Entities.SingletonEntities;
 using Core.Model.Systems;
 using PoorMansECS;
 using UnityEngine;
 
 namespace Core.Model {
-    public class SaveController {
-        public void Save(GameModel model) {
-            var entities = model.World.Entities.GetAllEntitiesFlatListed();
-            var saveJson = new Dictionary<string, object>();
-            foreach (var entity in entities) {
-                if (entity is ISerializable serializable) {
-                    serializable.Serialize(saveJson);
-                }
-            }
-            File.WriteAllText(Application.persistentDataPath, fastJSON.JSON.ToNiceJSON(saveJson));
-        }
-    }
     public class GameModel : MonoBehaviour {
         public World World { get; } = new();
         private readonly GameLoop _gameLoop = new();
@@ -41,18 +26,11 @@ namespace Core.Model {
             World.RemoveAllEntities();
         }
 
-        public void Save() {
-        }
-
         private void Update() {
             _gameLoop.Update(Time.deltaTime);
             if (UnityEngine.Input.GetKeyDown(KeyCode.H)) {
                 _saveController.Save(this);
             }
-        }
-
-        private void OnDestroy() {
-            Save();
         }
     }
 }
